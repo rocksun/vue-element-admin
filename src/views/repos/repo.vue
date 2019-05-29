a68728425e169204eaff0b7a05e8e07cb176c78e
rocksun <rocksun@rocksundeMBP.modouwifi.com>
2019-05-29 17:22:20 +0800

    last

---
+++ src/views/repos/repo.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-row>
+    <el-col />
+  </el-row>
+</template>
+
+<script>
+import { fetchList } from '@/api/repos'
+
+// , createRepo, updateRepo, deleteRepo
+import waves from '@/directive/waves' // Waves directive
+
+export default {
+  name: 'ReposManage',
+  directives: { waves },
+  filters: {},
+  data() {
+    return {}
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.edit-input {
+  padding-right: 100px;
+}
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+</style>
